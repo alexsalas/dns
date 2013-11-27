081cd02c42c4113fd852559c930a2c0641e3838b
Alexander Salas <a.salas@ieee.org>
2013-11-27 14:19:46 -0430

    DNS settings, more on: http://www.luadns.com/

---
+++ alexandersalas.name.lua
@@ -0,0 +1,8 @@
+-- File: example.com.lua
+-- Zone: example.com
+-- _a variable is replaced with zone name "example.com"
+
+-- ## GitHub pages example
+-- More info in "Custom Domains" section here: http://help.github.com/pages/
+a(_a, "204.232.175.78")
+cname("www", "alexsalas.github.com")
